16f4f3eebc378c73b66fb7dd308cb5e970f04910
miraksandak <ksandak.cz@gmail.com>
2023-11-07 15:29:59 +0100

    Upgrade drupal

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c42aeeeb0fc21de75ca324cc7d26b20c048fe1ed',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c42aeeeb0fc21de75ca324cc7d26b20c048fe1ed',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
